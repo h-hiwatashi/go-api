bed8f9df5a53b2cc3c2d6947be2819835d3206be
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2024-12-17 00:21:21 +0900

    docker-compose.ymlとmysql.sqlを更新し、ボリュームマウントの設定を追加。データベースとテーブルの作成スクリプトを修正。

--- mysql.sql
+++ mysql.sql
@@ -1,7 +1,8 @@
-drop table sessions;
-drop table users;
+CREATE DATABASE  IF NOT EXISTS `go_clean_arch` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `go_clean_arch`;
 
-create table users (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
   id         serial primary key,
   uuid       varchar(64) not null unique,
   name       varchar(255),
@@ -10,7 +11,8 @@ create table users (
   created_at timestamp not null
 );
 
-create table sessions (
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE `sessions` (
   id         serial primary key,
   uuid       varchar(64) not null unique,
   email      varchar(255),
