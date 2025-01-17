b7c6c77835a4d5808c657b21975b2518386f176e
h-hiwatashi <62828304+h-hiwatashi@users.noreply.github.com>
2025-01-17 18:40:54 +0900

    Update handlers.go

--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -7,20 +7,56 @@ import (
 
 // 他のパッケージからも参照可能な関数・変数・定数を作成するためには、その名前を大文字から始める必要があります
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Hello, world!\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Hello, world!\n")
+	} else {
+		// もし、req の中の Method フィールドが GET でなかったら
+		// Invalid method というレスポンスを、405 番のステータスコードと共に返す
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Article...\n")
+	} else {
+		// もし、req の中の Method フィールドが GET でなかったら
+		// Invalid method というレスポンスを、405 番のステータスコードと共に返す
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article List\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article List\n")
+	} else {
+		// もし、req の中の Method フィールドが GET でなかったら
+		// Invalid method というレスポンスを、405 番のステータスコードと共に返す
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Article No.1\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(w, "Article No.1\n")
+	} else {
+		// もし、req の中の Method フィールドが GET でなかったら
+		// Invalid method というレスポンスを、405 番のステータスコードと共に返す
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Nice…\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Nice…\n")
+	} else {
+		// もし、req の中の Method フィールドが GET でなかったら
+		// Invalid method というレスポンスを、405 番のステータスコードと共に返す
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Comment…\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(w, "Posting Comment…\n")
+	} else {
+		// もし、req の中の Method フィールドが GET でなかったら
+		// Invalid method というレスポンスを、405 番のステータスコードと共に返す
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
