904339fc482ba98140ab2df3923d8200cfc4f192
h-hiwatashi <62828304+h-hiwatashi@users.noreply.github.com>
2025-01-17 17:08:32 +0900

    Update main.go

--- app/main.go
+++ app/main.go
@@ -4,16 +4,20 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"github.com/h-hiwatashi/go-api/handlers"
 )
 
 func main() {
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
-	}
-	// 定義した helloHandler を使うように登録
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/hello", handlers.HelloHandler)
+	http.HandleFunc("/article", handlers.PostArticleHandler)
+	http.HandleFunc("/article/list", handlers.ArticleListHandler)
+	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	http.HandleFunc("/comment", handlers.PostCommentHandler)
+
 	// サーバー起動時のログを出力
 	log.Println("server start at port 8080")
+
 	// ListenAndServe 関数にて、サーバーを起動
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
