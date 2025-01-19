795dc0fb19f0f83465c8843c4a4454261a915603
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2025-01-19 15:57:12 +0900

    記事モデルとサンプルデータを追加し、記事およびコメントのハンドラーを修正してJSONレスポンスを返すように変更

--- app/handlers/handlers.go
+++ app/handlers/handlers.go
@@ -1,12 +1,14 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/h-hiwatashi/go-api/app/models"
 )
 
 // 他のパッケージからも参照可能な関数・変数・定数を作成するためには、その名前を大文字から始める必要があります
@@ -21,7 +23,15 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello, world!\n")
 }
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Article...\n")
+	article := models.Article1
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	// []byte 型である jsonData を
+	// string 型に変換することなくそのままレスポンス書き込みに使うことができる
+	w.Write(jsonData)
 }
 
 // • x が数字だった場合は、記事一覧ページの x ページ目に表示されるデータを返す
@@ -29,22 +39,29 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 // • x が数字ではなかった場合には、リクエストについていたパラメータの値が悪いということなので 400 番エラーを返す
 // • クエリパラメータが URL についていなかった場合には、パラメータ page=1 がついていたときと同じ処理をする
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
-	queryMap := req.URL.Query()
-	var page int
-	/// もし map 型の変数 queryMap が文字列"page"をキーに持っているのであれば、p には pageキーに対応する値 queryMap["page"] が、ok には true が格納される
-	/// もし map 型の変数 queryMap が文字列"page"をキーに持っていないのであれば、ok にはfalse が格納される
-	if p, ok := queryMap["page"]; ok && len(p) > 0 {
-		var err error
-		page, err = strconv.Atoi(p[0])
-		if err != nil {
-			http.Error(w, "Invalid page", http.StatusBadRequest)
-			return
-		}
-	} else {
-		page = 1
+	// queryMap := req.URL.Query()
+	// var page int
+	// /// もし map 型の変数 queryMap が文字列"page"をキーに持っているのであれば、p には pageキーに対応する値 queryMap["page"] が、ok には true が格納される
+	// /// もし map 型の変数 queryMap が文字列"page"をキーに持っていないのであれば、ok にはfalse が格納される
+	// if p, ok := queryMap["page"]; ok && len(p) > 0 {
+	// 	var err error
+	// 	page, err = strconv.Atoi(p[0])
+	// 	if err != nil {
+	// 		http.Error(w, "Invalid page", http.StatusBadRequest)
+	// 		return
+	// 	}
+	// } else {
+	// 	page = 1
+	// }
+	// reqString := fmt.Sprintf("Article List (page: %d)\n", page)
+
+	articleList := []models.Article{models.Article1, models.Article2}
+	jsonData, err := json.Marshal(articleList)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
 	}
-	reqString := fmt.Sprintf("Article List (page: %d)\n", page)
-	io.WriteString(w, reqString)
+	w.Write(jsonData)
 }
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	// articleID という変数に、リクエストの URL から取得した id パラメータを格納
@@ -57,10 +74,30 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	}
 	resString := fmt.Sprintf("Article No.%d\n", articleID)
 	io.WriteString(w, resString)
+
+	article := models.Article1
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
 }
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Nice…\n")
+	article := models.Article1
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
 }
 func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "Posting Comment…\n")
+	comment := models.Comment1
+	jsonData, err := json.Marshal(comment)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(jsonData)
 }
