fd431c5e2f68fe793400df8b08fd4e7ba9137f9e
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2025-02-06 20:54:55 +0900

    DB接続処理を関数に分割し、テストデータのセットアップおよびクリーンアップ処理を改善

--- app/repositories/main_test.go
+++ app/repositories/main_test.go
@@ -12,17 +12,20 @@ import (
 
 var testDB *sql.DB
 
-func setup() error {
-	// DBの接続
-	dbUser := "user"
-	dbPassword := "user"
-	dbDatabase := "go_api_mysql"
-	dbConn := fmt.Sprintf(
+// DB 接続部分もついでに別関数に切り出し
+var (
+	dbUser     = "user"
+	dbPassword = "user"
+	dbDatabase = "go_api_mysql"
+	dbConn     = fmt.Sprintf(
 		"%s:%s@tcp(localhost:3306)/%s?parseTime=true",
 		dbUser,
 		dbPassword,
 		dbDatabase,
 	)
+)
+
+func connectDB() error {
 	var err error
 	testDB, err = sql.Open("mysql", dbConn)
 	if err != nil {
@@ -31,7 +34,25 @@ func setup() error {
 	return nil
 }
 
+func setup() error {
+	// DBの接続
+	if err := connectDB(); err != nil {
+		return err
+	}
+	if err := cleanupDB(); err != nil {
+		fmt.Println("cleanup", err)
+		return err
+	}
+	if err := setupTestData(); err != nil {
+		fmt.Println("setup")
+		return err
+	}
+	return nil
+}
+
+// テスト共通の後処理
 func teardown() {
+	cleanupDB()
 	testDB.Close()
 }
 
@@ -47,19 +68,20 @@ func TestMain(m *testing.M) {
 	teardown()
 }
 
+// mysql コマンドを用いて、setupDB.sql と cleanupDB.sql ファイルに書いたクエリを実行
 func setupTestData() error {
-	cmd := exec.Command("mysql", "-h", "127.0.0.1", "-u", "docker", "sampledb",
-		"--password=docker", "-e", "source ./testdata/setupDB.sql")
+	cmd := exec.Command("mysql", "-h", "127.0.0.1", "-u", "user", "go_api_mysql",
+		"--password=user", "-e", "source ./testdata/setupDB.sql")
 	err := cmd.Run()
 	if err != nil {
 		return err
 	}
 	return nil
 }
-func cleanupDB() error {
-	cmd := exec.Command("mysql", "-h", "127.0.0.1", "-u", "docker", "sampledb",
 
-		"--password=docker", "-e", "source ./testdata/cleanupDB.sql")
+func cleanupDB() error {
+	cmd := exec.Command("mysql", "-h", "127.0.0.1", "-u", "user", "go_api_mysql",
+		"--password=user", "-e", "source ./testdata/cleanupDB.sql")
 	err := cmd.Run()
 	if err != nil {
 		return err
