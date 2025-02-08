84534e14cf26975d324c0f2787bbb3ac140a8c52
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2025-02-08 23:59:25 +0900

    サービス層に記事取得機能を追加し、記事詳細と関連コメントを取得する処理を実装

---
+++ app/services/article_service.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"github.com/h-hiwatashi/go-api/app/models"
+	"github.com/h-hiwatashi/go-api/app/repositories"
+)
+
+// 指定IDの記事をDBから取得する関数
+func GetArticleService(articleID int) (models.Article, error) {
+	article, err := repositories.SelectArticleDetail(db, articleID)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	// SelectArticleDetail 関数では「指定 ID 記事に紐づいたコメント一覧」までは取得できないため、
+	// SelectCommentList 関数を実行する
+	commentList, err := repositories.SelectCommentList(db, articleID)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	article.CommentList = append(article.CommentList, commentList...)
+	return article, nil
+}
