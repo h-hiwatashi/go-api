0111d5e5d13603a9d32fc353e7356f0f6e27e821
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2025-02-10 00:14:11 +0900

    コメント作成サービスを追加し、新しいコメントをデータベースに挿入する機能を実装

---
+++ app/services/comment_service.go
@@ -0,0 +1,23 @@
+package services
+
+import (
+	"github.com/h-hiwatashi/go-api/app/models"
+	"github.com/h-hiwatashi/go-api/app/repositories"
+)
+
+// PostCommentHandler で使用することを想定したサービス
+// 引数の情報をもとに新しいコメントを作り、結果を返却
+func PostCommentService(comment models.Comment) (models.Comment, error) {
+	db, err := connectDB()
+	if err != nil {
+		return models.Comment{}, err
+	}
+	defer db.Close()
+
+	newComment, err := repositories.InsertComment(db, comment)
+	if err != nil {
+		return models.Comment{}, err
+	}
+
+	return newComment, nil
+}
