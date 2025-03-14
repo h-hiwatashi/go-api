b8cd404a8f39c92baee0bf9c5e11a71c80eb071a
h-hiwatashi <hiwatashi.hiromitsu@gmail.com>
2025-03-14 23:56:01 +0900

    MyAppErrorにエラーメッセージをラップする新しいWrapメソッドを追加し、エラーコードにInsertDataFailedを追加

--- app/apperrors/errorcode.go
+++ app/apperrors/errorcode.go
@@ -3,7 +3,6 @@ package apperrors
 type ErrCode string
 
 const (
-	Unknown              ErrCode = "U000"
-	ErrCodeInternalError ErrCode = "INTERNAL_ERROR"
-	ErrCodeBadRequest    ErrCode = "BAD_REQUEST"
+	Unknown          ErrCode = "U000"
+	InsertDataFailed ErrCode = "S001"
 )
